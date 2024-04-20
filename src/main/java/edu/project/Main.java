feb10ca75cd33030318614eb003c298dfcb5634a
daawaan4U <theoneeclarin@outlook.com>
2024-04-20 21:40:56 +0800

    fix(theme): use flat MacOS light, suits better with satellite imagery

--- src/main/java/edu/project/Main.java
+++ src/main/java/edu/project/Main.java
@@ -2,14 +2,14 @@ package edu.project;
 
 import javax.swing.SwingUtilities;
 
-import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
 
 import edu.project.components.App;
 
 public class Main {
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> {
-			FlatLightLaf.setup();
+			FlatMacLightLaf.setup();
 
 			App app = new App();
 			app.setVisible(true);
