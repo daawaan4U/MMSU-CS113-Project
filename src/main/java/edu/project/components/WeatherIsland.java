91cb92f49b9c60e17db58e2d1d0555fa64f6d367
daawaan4U <theoneeclarin@outlook.com>
2024-05-21 11:16:34 +0800

    add rounded corners on `WeatherIsland`

--- src/main/java/edu/project/components/WeatherIsland.java
+++ src/main/java/edu/project/components/WeatherIsland.java
@@ -1,5 +1,9 @@
 package edu.project.components;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -10,9 +14,19 @@ import edu.project.Context;
 public class WeatherIsland extends JPanel {
 	public WeatherIsland(Context context) {
 		putClientProperty(FlatClientProperties.STYLE,
-				"background: tint(@background,50%); border: 16,16,16,16,shade(@background,10%),,0");
+				"background: tint(@background,50%); border: 16,16,16,16,shade(@background,10%),,16");
+		setOpaque(false);
 
 		JLabel label = new JLabel("Hello World!");
 		add(label);
+	}
+
+	@Override
+	protected void paintComponent(Graphics graphics) {
+		Graphics2D graphics2d = (Graphics2D) graphics;
+		graphics2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		graphics2d.setColor(getBackground());
+		graphics2d.fillRoundRect(0, 0, getWidth(), getHeight(), 16 + 8, 16 + 8);
+		super.paintComponent(graphics2d);
 	}
 }
